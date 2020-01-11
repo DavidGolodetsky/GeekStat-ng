b726ac46b720527890c6ce997fd8fd3d0df952c6
David Golodetsky <davyd.lito@gmail.com>
2020-01-11 14:45:44 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCKGs3CuCivnG0_DZgLFd0cs1XrROv9PSI",
+    authDomain: "geekstat-5d017.firebaseapp.com",
+    databaseURL: "https://geekstat-5d017.firebaseio.com",
+    projectId: "geekstat-5d017",
+    storageBucket: "geekstat-5d017.appspot.com",
+    messagingSenderId: "390923711624",
+    appId: "1:390923711624:web:b167afb386ce12aa7e5b3d",
+    measurementId: "G-CEYEQRYQP1"
+  }
 };
